8a59ee605b03c2260aa433263b2ce0c69703b9f7
Nanton-Kijpadung <nanton.lian@gmail.com>
2026-02-07 14:49:45 +0700

    Delete ta-tip---student-focus-monitoring directory

--- ta-tip---student-focus-monitoring/components/Logo.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-const Logo: React.FC<{ className?: string }> = ({ className = "w-10 h-10" }) => (
-    <img src="tatip_logo.png" alt="Ta-Tip Logo" className={`${className} object-contain`} />
-);
-
-export default Logo;
